a712076923ae3016c3e75f458336086b50f85d84
TarasOliinyk <taras.oliynuk@gmail.com>
2020-04-23 14:03:04 +0300

    Unit tests for CourseService

---
+++ src/test/java/com/springboot/practice/service/TeacherServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.springboot.practice.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TeacherServiceImplTest {
+
+    // ToDo.
+}
